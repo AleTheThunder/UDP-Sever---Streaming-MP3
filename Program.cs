a0c1df4c20bf3110f7cb7612e24381017513b702
Alexander Donner <donneralexander22@gmx.at>
2023-11-01 22:14:31 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace UDP_Sever
+{
+    class Program
+    {
+        static void Main()
+        {
+            UdpClient udpServer = new UdpClient();
+            IPEndPoint clientEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);
+
+            try
+            {
+                Console.WriteLine("Streaming gestartet...");
+
+                // Öffne die Videodatei
+                FileStream videoFileStream = File.OpenRead("D:/Schule/MyStack/video/test.mp3");
+
+                byte[] buffer = new byte[1024];
+
+                int bytesRead;
+                while ((bytesRead = videoFileStream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    // Sende Frame mit Sequenznummer
+                    udpServer.Send(buffer, bytesRead, clientEndPoint);
+
+                    // Kurze Pause zwischen den Frames (simuliert die Übertragungsgeschwindigkeit)
+                    System.Threading.Thread.Sleep(50);
+                }
+
+                Console.WriteLine("Streaming beendet.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                udpServer.Close();
+            }
+        }
+    }
+}
